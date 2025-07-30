3a3271279f79475119b8fc95aacc677758db17c2
ThiagoLopesSantos <thiago8343@gmail.com>
2025-07-30 18:45:30 -0300

    construindo a estrutura e a seção 'sobre mim'

---
+++ src/containers/Sidebar/index.tsx
@@ -0,0 +1,10 @@
+import Titulo from '../../components/Titulo'
+
+const Sidebar = () => (
+  <aside>
+    <img src="https://github.com/thiagolopessantos.png" />
+    <Titulo fontSize={20}>Thiago Lopes</Titulo>
+  </aside>
+)
+
+export default Sidebar
---
+++ src/containers/Sobre/styles.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+export const GithubSecao = styled.div`
+  margin-top: 32px;
+  margin-bottom: 32px;
+  img {
+    height: 157px;
+  }
+`
