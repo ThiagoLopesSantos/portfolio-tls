e6524ffd47f2fe56d50b2c6b5814f14eb204e601
ThiagoLopesSantos <thiago8343@gmail.com>
2025-07-30 19:45:53 -0300

    estilizando a sidebar

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,21 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+import { Descricao, BotaoTema, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/thiagolopessantos.png" />
-    <Titulo fontSize={18}>Thiago Lopes</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={18}>Thiago Lopes</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        thiagolopessantos
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Engenheiro Front-end
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
